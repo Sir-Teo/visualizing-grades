a8436d8ab73c64d7e4037a5253cae0df275e3d02
Teo <32597645+Sir-Teo@users.noreply.github.com>
2022-12-07 17:00:07 -0800

    changed labels

--- draw.js
+++ draw.js
@@ -1,45 +1,10 @@
-var gradesData;
-
-d3.csv("grades.csv", function(data) {
-gradesData = data;
-
-// Parse the data and convert strings to numbers
-gradesData.forEach(function(d) {
-d.Quarter = +d.Quarter;
-d.Course_Level2 = +d.Course_Level2;
-d.Course = +d.Course;
-d.Instructor = +d.Instructor;
-d.Grade_Given = +d.Grade_Given;
-d.Sum_of_Student_Count = +d.Sum_of_Student_Count;
-});
-
-// Set up scales and axes
-var x = d3.scaleLinear()
-.domain([0, d3.max(gradesData, function(d) { return d.Quarter; })])
-.range([0, width]);
-var y = d3.scaleLinear()
-.domain([0, d3.max(gradesData, function(d) { return d.Grade_Given; })])
-.range([height, 0]);
-var xAxis = d3.axisBottom(x);
-var yAxis = d3.axisLeft(y);
-
-// Add axes to the chart
-svg.append("g")
-.attr("class", "x axis")
-.attr("transform", "translate(0," + height + ")")
-.call(xAxis);
-svg.append("g")
-.attr("class", "y axis")
-.call(yAxis);
-
-// Add the data points to the chart
-svg.selectAll(".dot")
-.data(gradesData)
-.enter().append("circle")
-.attr("class", "dot")
-.attr("r", 3.5)
-.attr("cx", function(d) { return x(d.Quarter); })
-.attr("cy", function(d) { return y(d.Grade_Given); });
-});
-
-
+function main() {
+	// read grades.csv file using d3
+    d3.csv("grades.csv", function(data) {
+        for (var i = 0; i < 10; i++) {
+            console.log(data[i].Quarter);
+            console.log(data[i].Course);
+        }
+    });
+}
+main();
